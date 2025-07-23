adeb899c546b387d17224f7450d6790612b611d1
Dylan Maxwell <113061279+portgasdylan@users.noreply.github.com>
2025-07-23 13:36:40 -0700

    Create secrets2.h

---
+++ Final/secrets2.h
@@ -0,0 +1,8 @@
+#define SECRET_WRITE_APIKEY "YTDBMXQ70BDVRMN8"
+#define SECRET_CH_ID 2996294
+
+#define SECRET_SSID "Scytale"
+#define SECRET_PASS "Chaumurky26"
+
+#define FIREBASE_API_KEY "AIzaSyD0PzgDZ7IomHrJuGsNRYs2QpeinyYAY7k"
+#define FIREBASE_DATABASE_URL "https://csc230twice-default-rtdb.firebaseio.com/"
